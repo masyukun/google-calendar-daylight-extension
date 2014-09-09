90df6cc057a8d2f83eb634c5c90fb309ece69f6c
Matthew Royal <masyukun@gmail.com>
2014-09-09 01:18:46 -0400

    Made sure background process only injects once.

--- background.js
+++ background.js
@@ -1,12 +1,21 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status !== 'complete')
-    {
-        return;
+window.didIt = false;
+window.injectedAlready = false;
+
+if (!window.didIt) {
+    window.didIt = true;
+    if (chrome.tabs) {
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+            if (changeInfo.status !== 'complete') {
+                return;
+            }
+            if (!window.injectedAlready) {
+                window.injectedAlready = true;
+                var match = 'https://www.google.com/calendar/render';
+                if (tab.url.substring(0, match.length) === match) {
+                	chrome.tabs.executeScript(tabId, { file: 'jquery.min.js' });
+                    chrome.tabs.executeScript(tabId, { file: 'daylight.js' });
+                }
+            }
+        });
     }
-    var match = 'https://www.google.com/calendar/render';
-    if (tab.url.substring(0, match.length) === match)
-    {
-    	chrome.tabs.executeScript(tabId, { file: 'jquery.min.js' });
-        chrome.tabs.executeScript(tabId, { file: 'daylight.js' });
-    }
-});
+}
